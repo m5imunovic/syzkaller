43523787cd329bd8c5b7a8a6affe96abb764a1d8
m5imunovic <simunovic.marijo@gmail.com>
2020-11-16 13:24:04 +0100

    tools/syz-showprio/showprio.go: new flag for exporting corpus into CSV database

--- tools/syz-showprio/showprio.go
+++ tools/syz-showprio/showprio.go
@@ -5,14 +5,18 @@
 package main
 
 import (
+	"bytes"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 
 	"github.com/google/syzkaller/pkg/db"
 	"github.com/google/syzkaller/pkg/mgrconfig"
+	"github.com/google/syzkaller/pkg/osutil"
 	"github.com/google/syzkaller/prog"
 )
 
@@ -21,6 +25,7 @@ var (
 	flagArch   = flag.String("arch", runtime.GOARCH, "target arch")
 	flagEnable = flag.String("enable", "", "comma-separated list of enabled syscalls")
 	flagCorpus = flag.String("corpus", "", "name of the corpus file")
+	flagExport = flag.String("csv", "", "name of CSV output for corpus export")
 )
 
 func main() {
@@ -40,10 +45,14 @@ func main() {
 		fmt.Fprintf(os.Stderr, "failed to parse enabled syscalls: %v", err)
 		os.Exit(1)
 	}
+	fmt.Println(*flagCorpus)
 	corpus, err := db.ReadCorpus(*flagCorpus, target)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to read corpus: %v", err)
 		os.Exit(1)
+	}
+	if *flagExport != "" {
+		exportSyscalls(*flagExport, corpus)
 	}
 	showPriorities(enabled, target.CalculatePriorities(corpus), target)
 }
@@ -66,4 +75,32 @@ func printLine(values []string) {
 		fmt.Printf("%-20v|", val)
 	}
 	fmt.Printf("\n")
+}
+
+func exportSyscalls(csvname string, corpus []*prog.Prog) {
+	w := new(bytes.Buffer)
+	writer := csv.NewWriter(w)
+	defer writer.Flush()
+	var data [][]string
+	for pidx, p := range corpus {
+		callArgVal, callRetVal := p.SerializeArgRet(false)
+		for cidx, call := range p.Calls {
+			m := call.Meta
+			data = append(data, []string{
+				strconv.Itoa(pidx),
+				strconv.Itoa(cidx),
+				strconv.Itoa(m.ID),
+				strconv.FormatUint(m.NR, 10),
+				m.Name,
+				m.CallName,
+				callArgVal[cidx],
+				callRetVal[cidx],
+			})
+			fmt.Println(pidx, p.String(), cidx, m.ID, m.NR, m.Name, m.CallName)
+		}
+	}
+	writer.WriteAll(data)
+	if err := osutil.WriteFile("corpus.csv", w.Bytes()); err != nil {
+		fmt.Printf("%v", err)
+	}
 }
